1e36be3bcd81de67714eabb730701c886d508310
Salim Vazquez Solis <salimvzqz@gmail.com>
2022-08-14 17:17:40 -0500

    Refactor methods in routes/api and update README

--- README.md
+++ README.md
@@ -8,6 +8,26 @@ Luego de clonar el proyecto, necesitara:
     ```
     pip install -r requirements.txt
     ```
+- Cree una base de datos en su servicio de MongoDB.
+- Importe el archivo `data/data.json` en una colección llamada **contacts**.
+- Duplique el archivo `.env.example` y renombre la copia a `.env`
+    - Ingrese los datos y credenciales solicitadas.
+        ```
+        # Entorno en el que correra el proyecto
+        APP_ENV=local
+        # Host o IP en el que desea levantar el servidor
+        APP_HOST=localhost
+        # Puerto en el que desea levantar el servidor
+        APP_PORT=8000
+
+        # URI de su conexión a MongoDB
+        DB_HOST=mongodb://user:pass@host/
+        # Puerto en el que esta corriendo su instancia de MongoDB
+        DB_PORT=27017
+        # Nombre de la base de datos
+        DB_NAME=
+        ```
+
 
 ### 🌐 Ejecutar
 Deberas ejecutar en una bash
--- routes/api.py
+++ routes/api.py
@@ -1,35 +1,16 @@
-import json
 from typing import List, Union
-from uuid import uuid1
 from fastapi import APIRouter, HTTPException
 from models.contact import Contact
 from config.database import create_connection
 
 routes = APIRouter(tags=["API Contacts"])
 
-def fetch_data_file():
-    try:
-        with open('./data/data.json', 'r') as data_file:
-            contacts = json.load(data_file)
-    except FileNotFoundError:
-        return HTTPException(status_code=500, detail="File not found")
-    except json.JSONDecodeError:
-        return HTTPException(status_code=500, detail="Error at read the file")
-    finally:
-        data_file.close()
-        return contacts
-
-def add_data_file(peoples):
-    try:
-        with open('./data/data.json', 'w') as data_file:
-            json.dump(peoples, data_file)
-    except FileNotFoundError:
-        return HTTPException(status_code=500, detail="File not found")
-    except json.JSONDecodeError:
-        return HTTPException(status_code=500, detail="Error at read the file")
-    finally:
-        data_file.close()
-        return True
+def get_contact_by_id(contact_id):
+    contact = create_connection()["contacts"].find_one({'id': contact_id}, {'_id': 0})
+    if contact == None:
+        raise HTTPException(status_code=404,
+            detail=f"Contact with id {contact_id} does not exist!")
+    return contact
 
 @routes.get('/contacts', status_code=200, response_model=List[Contact])
 async def index(phrase: Union[str, None] = None):
@@ -53,22 +34,14 @@ async def store(contact: Contact):
 
 @routes.get('/contacts/{contact_id}', status_code=200)
 async def show(contact_id: str):
-    contact = create_connection()["contacts"].find_one({'id': contact_id}, {'_id': 0})
-    if contact == None:
-        raise HTTPException(status_code=404,
-            detail=f"Contact with id {contact_id} does not exist!")
+    contact = get_contact_by_id(contact_id)
     return contact
 
 @routes.patch('/contacts/{contact_id}', status_code=200)
 async def update(contact_id: str, contact_to_update: Contact):
-    contact = create_connection()["contacts"].find_one({'id': contact_id}, {'_id': 0})
-    if contact == None:
-        raise HTTPException(status_code=404,
-            detail=f"Contact with id {contact_id} does not exist!")
     contact_to_update.id = contact_id
     contact_updated = create_connection()["contacts"].find_one_and_update({"id": contact_id},
         { "$set":dict(contact_to_update)}, {'_id': 0})
-    print(contact_updated)
     if contact_to_update:
         return {"message": f"Contact with id: {contact_to_update.id} was updated", "data": contact_updated}
     else:
@@ -77,10 +50,6 @@ async def update(contact_id: str, contact_to_update: Contact):
 
 @routes.delete('/contacts/{contact_id}', status_code=204)
 async def destroy(contact_id: str):
-    contact = create_connection()["contacts"].find_one({'id': contact_id}, {'_id': 0})
-    if contact == None:
-        raise HTTPException(status_code=404,
-            detail=f"Contact with id {contact_id} does not exist!")
     create_connection()["contacts"].find_one_and_delete({'id': contact_id}, {'_id': 0})
     contact_exists = create_connection()["contacts"].find_one({'id': contact_id})
     if contact_exists == None:
