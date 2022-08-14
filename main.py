095f9cddc678e46c09af609a457e3f000b484074
Salim Vazquez Solis <salimvzqz@gmail.com>
2022-08-13 23:27:20 -0500

    Environment variables added into app

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 import uvicorn
 from fastapi import FastAPI
 from routes import api
+import os
+from dotenv import load_dotenv
 
 app = FastAPI()
-
+load_dotenv(dotenv_path='./.env')
 app.include_router(api.routes)
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="localhost", port=8000, reload=True)
+    uvicorn.run(
+        "main:app",
+        host=os.getenv("APP_HOST", "localhost"),
+        port=int(os.getenv("APP_PORT", 8000)),
+        reload=True if os.getenv("APP_ENV") == 'local' else False
+    )
